eebac07c5fa7ddb6c0209789fac3684cbac32728
Helge Drews <dev@hdrews.de>
2024-03-09 18:39:06 +0100

    chore: update commitlint config to avoid ts error on import

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,14 +3,9 @@
  * @see https://commitlint.js.org/reference/configuration.html
  */
 import type { UserConfig } from '@commitlint/types';
-import { RuleConfigSeverity } from '@commitlint/types';
 
 const Configuration: UserConfig = {
-
-  extends: ['@commitlint/config-conventional'],
-  // parserPreset: 'conventional-changelog-conventionalcommits',
-  // formatter: '@commitlint/format',
-  // rules: {},
+  extends: ['@commitlint/config-conventional']
 };
 
 export default Configuration;
