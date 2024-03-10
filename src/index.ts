33fe4ea6122aeef007a859f01f0386f679c32f46
Helge Drews <dev@hdrews.de>
2024-03-10 11:38:05 +0100

    chore: test configuration to import module into html file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { DateTimeActions } from "./types/index.js";
+
+export function initDateTimeActions(): DateTimeActions {
+    console.log("[hdos-date-time-actions] Initialize");
+    return 'DateTimeActions';
+}
