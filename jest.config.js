c53f874386c56542c4e07e47ad326d811173f115
Helge Drews <dev@hdrews.de>
2024-03-10 11:32:50 +0100

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// https://kulshekhar.github.io/ts-jest/docs/getting-started/installation
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
