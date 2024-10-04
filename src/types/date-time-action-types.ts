a138c79226aefec2b3d41ab33e7c39a68235fb51
Helge Drews <dev@hdrews.de>
2024-10-04 21:12:18 +0200

    feat: add more possible actions to DateTimeActions

--- src/types/date-time-action-types.ts
+++ src/types/date-time-action-types.ts
@@ -1,1 +1,13 @@
-export type DateTimeActions = 'DateTimeActions';
+export interface DateTimeActions {
+    hideElementsAfterDate: () => void;
+    showElementsAfterDate: () => void;
+    toggleElementsAtDate: (date: Date) => void;
+    addClassAtDate: (className: string, date: Date) => void;
+    removeClassAtDate: (className: string, date: Date) => void;
+    executeCallbackAtDate: (callback: () => void, date: Date) => void;
+    animateElementsAtDate: (animationName: string, date: Date) => void;
+    fetchDataAtDate: (url: string, date: Date) => Promise<any>;
+    updateContentAtDate: (selector: string, content: string, date: Date) => void;
+    scheduleRecurringAction: (action: () => void, schedule: string) => void;
+    cancelScheduledAction: (actionId: string) => void;
+}
